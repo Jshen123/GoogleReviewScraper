5dcec58007997b85c1d855be7d6fe7d7cf6131c8
Jshen123 <joeshen123@gmail.com>
2018-05-12 13:52:51 -0700

    build google review scraper constructor

--- src/io/github/jshen123/googlereviewscrapper/GoogleReviewScrapper.java
+++ src/io/github/jshen123/googlereviewscrapper/GoogleReviewScrapper.java
@@ -1,11 +1,21 @@
 package io.github.jshen123.googlereviewscrapper;
 
+import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class GoogleReviewScrapper {
+    private Document document;
 
+    public GoogleReviewScrapper(String url){
+        try{
+            document = Jsoup.connect(url).get();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
 
 }
