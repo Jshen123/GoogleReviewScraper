2a76c3234a4169aeef72ca1b719baef2204c5180
Jshen123 <joeshen123@gmail.com>
2018-05-12 16:28:04 -0700

    modify the scraper input from String and ArrayList; modify test input in main

--- src/io/github/jshen123/googlereviewscraper/GoogleReviewScraper.java
+++ src/io/github/jshen123/googlereviewscraper/GoogleReviewScraper.java
@@ -2,39 +2,45 @@ package io.github.jshen123.googlereviewscraper;
 
 import java.io.IOException;
 
+import java.util.ArrayList;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class GoogleReviewScraper {
-    private Elements rating;
-    private Elements numberOfReviews;
-    private Document document;
+    private ArrayList<Document> document = new ArrayList<>();
+    private ArrayList<String> rating = new ArrayList<>();
+    private ArrayList<String> numberOfReviews = new ArrayList<>();
 
-    public GoogleReviewScraper(String url){
+    public GoogleReviewScraper(ArrayList<String> urlList){
         try{
-            // get Document object after parsing the html from given url
-            document = Jsoup.connect(url).get();
-            // get rating from the specified url
-            rating = document.select("span.rtng");
-            // get number of reviews from the specified url
-            numberOfReviews = document.select("span:containsOwn( Google reviews)");
-
+            for (int i = 0; i < urlList.size(); i++){
+                // get Document object after parsing the html from given url
+                document.add(Jsoup.connect(urlList.get(i)).get());
+                // get rating from the specified url and convert the rating from element to string
+                rating.add(document.get(i).select("span.rtng").text());
+                // get nnumber of reviews from the specified url and convert the rating from element to string
+                numberOfReviews.add(convertElementsToString(document.get(i).select("span:containsOwn( Google reviews)")));
+            }
         } catch (IOException e){
             e.printStackTrace();
         }
     }
 
-    public Document getDocument() {
-        return document;
+
+    public String convertElementsToString (Elements element){
+        String string = element.text();
+        String split[] = string.split(" ", 2);
+        return split[0];
+
     }
 
-    public Elements getRating() {
+    public ArrayList<String> getRating() {
         return rating;
     }
 
-    public Elements getNumberOfReviews() {
+    public ArrayList<String> getNumberOfReviews() {
         return numberOfReviews;
     }
-
 }
--- src/io/github/jshen123/googlereviewscraper/Main.java
+++ src/io/github/jshen123/googlereviewscraper/Main.java
@@ -1,10 +1,24 @@
 package io.github.jshen123.googlereviewscraper;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String args[]){
         print("running...");
 
-        GoogleReviewScraper scraper = new GoogleReviewScraper("www.google.com");
+        ArrayList<String> testArray = new ArrayList<>();
+        testArray.add("https://www.google.com/search?q=900+Arbour+Lake+Road+NW+senior");
+        testArray.add("https://www.google.com/search?q=Brooksdale+Broadway+Mesa+senior");
+
+        GoogleReviewScraper scraper = new GoogleReviewScraper(testArray);
+
+//        for (int i = 0; i < testArray.size(); i++){
+//            print(scraper.getRating().get(i));
+//            print(scraper.getNumberOfReviews().get(i));
+//        }
+
+
+        print("done!");
     }
 
     public static void print(String string) {
