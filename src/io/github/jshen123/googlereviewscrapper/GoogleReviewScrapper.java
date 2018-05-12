2c37d225b3b86528ef9cd86d9a5d104b10049441
Jshen123 <joeshen123@gmail.com>
2018-05-12 13:36:00 -0700

    import jsoup functions

--- src/io/github/jshen123/googlereviewscrapper/GoogleReviewScrapper.java
+++ src/io/github/jshen123/googlereviewscrapper/GoogleReviewScrapper.java
@@ -1,4 +1,11 @@
 package io.github.jshen123.googlereviewscrapper;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 public class GoogleReviewScrapper {
+
+
+
 }
