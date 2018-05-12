58ed943fbb575fbe1434c48eea4a8fdcc7b2e619
Jshen123 <joeshen123@gmail.com>
2018-05-12 14:51:16 -0700

    implement rating and getNumberOfReviews variables and respective getter function

--- src/io/github/jshen123/googlereviewscraper/GoogleReviewScraper.java
+++ src/io/github/jshen123/googlereviewscraper/GoogleReviewScraper.java
@@ -7,11 +7,19 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class GoogleReviewScraper {
+    private Elements rating;
+    private Elements numberOfReviews;
     private Document document;
 
     public GoogleReviewScraper(String url){
         try{
+            // get Document object after parsing the html from given url
             document = Jsoup.connect(url).get();
+            // get rating from the specified url
+            rating = document.select("span.rtng");
+            // get number of reviews from the specified url
+            numberOfReviews = document.select("span:containsOwn( Google reviews)");
+
         } catch (IOException e){
             e.printStackTrace();
         }
@@ -20,4 +28,13 @@ public class GoogleReviewScraper {
     public Document getDocument() {
         return document;
     }
+
+    public Elements getRating() {
+        return rating;
+    }
+
+    public Elements getNumberOfReviews() {
+        return numberOfReviews;
+    }
+
 }
