8d54ae1a353b324e210cef9c9e17126e8695a23c
Jshen123 <joeshen123@gmail.com>
2018-05-12 18:21:10 -0700

    implement feature to scan rows from csv file to items in ArrayList

--- src/io/github/jshen123/googlereviewscraper/Main.java
+++ src/io/github/jshen123/googlereviewscraper/Main.java
@@ -1,18 +1,44 @@
 package io.github.jshen123.googlereviewscraper;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String args[]){
-        print("running...");
 
-        ArrayList<String> testArray = new ArrayList<>();
-        testArray.add("https://www.google.com/search?q=900+Arbour+Lake+Road+NW+senior");
-        testArray.add("https://www.google.com/search?q=Brooksdale+Broadway+Mesa+senior");
+        print("Enter csv file location: ");
+        Scanner reader = new Scanner(System.in);
 
-        GoogleReviewScraper scraper = new GoogleReviewScraper(testArray);
+        ArrayList<String> urlList= new ArrayList<>();
 
-//        for (int i = 0; i < testArray.size(); i++){
+        String fileName = reader.nextLine();
+        File file = new File(fileName);
+
+        try {
+            Scanner inputStream = new Scanner(file);
+            while(inputStream.hasNextLine()){
+                urlList.add("https://www.google.com/search?q=" + inputStream.nextLine());
+            }
+            inputStream.close();
+
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        urlList.remove(0);
+
+//        print("running...");
+
+//        ArrayList<String> testArray = new ArrayList<>();
+//        testArray.add("https://www.google.com/search?q=900+Arbour+Lake+Road+NW+senior");
+//        testArray.add("https://www.google.com/search?q=Brooksdale+Broadway+Mesa+senior");
+
+        GoogleReviewScraper scraper = new GoogleReviewScraper(urlList);
+//
+//        for (int i = 0; i < urlList.size(); i++){
 //            print(scraper.getRating().get(i));
 //            print(scraper.getNumberOfReviews().get(i));
 //        }
